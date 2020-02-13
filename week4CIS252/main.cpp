fe22b874cb05ce471ef292f230cc9f4a6d4e9780
Carol Sanders <carolsand@gmail.com>
2020-02-12 19:26:12 -0800

    checking in new files to master

---
+++ week4CIS252/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class unoR3 {
+private:
+    int arr[10];
+
+public:
+    int getArr(i) { return i; }
+
+    int setArr(i) { return i; }
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
