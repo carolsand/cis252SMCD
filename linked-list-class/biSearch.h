953651299caa1e3c26eb942f52bb718db39ecedf
Carol Sanders <cas-labs@outlook.com>
2020-02-15 20:07:17 -0800

    practicing git

--- linked-list-class/biSearch.h
+++ linked-list-class/biSearch.h
@@ -6,3 +6,28 @@
 #define CIS252SMCD_BISEARCH_H
 
 #endif //CIS252SMCD_BISEARCH_H
+
+int binSearch(int key, int node) {
+    current_node = node;
+    while (current_node != NULL){
+        if (key == current_node){
+            return current_node;
+        }
+        if (key < current_node){
+            current_node = current_node.left
+        }
+        else {
+            current_node = current_node.right
+        }
+    }
+    return current_node
+}
+
+int findValue(int first, int last, int val){
+    while( first != last){
+        if(*first == val)
+            return first;
+            first ++;
+    }
+    return last;
+}
