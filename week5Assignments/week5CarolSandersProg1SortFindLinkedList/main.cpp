d5c6ebd863a074830e49a25b893ab091f1c29ddb
Carol Sanders <cas-labs@outlook.com>
2020-02-16 18:28:13 -0800

    Adding mergeSort to LINKED-LIST. Messy

--- week5Assignments/week5CarolSandersProg1SortFindLinkedList/main.cpp
+++ week5Assignments/week5CarolSandersProg1SortFindLinkedList/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdc++>
 using namespace std;
 
 class NODE {
@@ -11,6 +12,8 @@ public:
 class LIST {
 public:
     NODE * ptrHead;  // Start of linked list
+    NODE* SortedMerge(NODE* a, NODE* b);
+    void FrontBackSplit(NODE* source, NODE** frontRef, NODE** backRf);
     void add(int inData);
     bool remove (int inData);
     bool isEmpty();
@@ -18,6 +21,7 @@ public:
     bool search(int inData);
     void printAll();
     void userInput();
+    void mergeSort(NODE** headRef);
     LIST() { ptrHead = NULL; nodeCount = 0;}
 
 private:
@@ -65,7 +69,7 @@ bool LIST::remove(int inData) {
 
             if (ptrCurrent->data == inData) {
                 ptrPrevious->ptrNext = ptrCurrent->ptrNext; // bypass node
-                delete ptrCurrent; // delete bypadded node
+                delete ptrCurrent; // delete bypassed node
                 nodeCount--;
                 return true;
             }
@@ -129,9 +133,76 @@ void LIST::userInput(){
     }
 }
 
+/* sorts the linked list by changing next pointers (Not contents of bucket) */
+void LIST::mergeSort(NODE** headRef) {
+    /* Base Case */
+    NODE *ptrCurrent = ptrHead; // start at the beginning
+    NODE* head = *headRef;
+    NODE* a;
+    NODE* b;
+    /* Base Case ---length 0 or 1 */
+    if ((head == NULL) || (head -> == NULL)) {
+        return;
+    }
+    /* Split head into two lists a and b */
+    FrontBackSplit(head, &a, &b);
+
+    /* Recursively sort the sub-lists a and b */
+    mergeSort(&a);
+    mergeSort(&b);
+
+    /* Result = merging two sorted lists together */
+    *headRef = SortedMerge(a, b);
+}
+
+void moveNode(NODE** desRef, NODE** sourceRef);
+    int next;
+
+    NODE* SortedMerge(NODE* a, NODE* b)
+{
+    NODE* result = NULL;
+    /* Base Cases*/
+    if (a == NULL){
+        return (b);
+    }
+    else if(b == NULL){
+        return a;
+    }
+    /* Pick either a or b and recurse */
+    if (a->data <= b->data) {
+        result = a;
+        result ->next = SortedMerge(a->next, b);
+    } else {
+        result = b;
+        result->next = SortedMerge(a, b->next);
+    }
+    return result;
+}
+
+// Utility Function to split the given list
+void FrontBackSplit(NODE* source, NODE** frontRef, NODE** backRef){
+    NODE* fast;
+    NODE* slow;
+    slow = source;
+    fast = source ->next;
+    /* Advance two nodes or one node */
+    while (fast != NULL){
+        fast = fast->next;
+        if(fast != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+    /* slow is before the midpoint in the list, so spilt it in two at the point */
+    *frontRef = source;
+    *backRef = slow->next;
+    slow->next = NULL;
+}
+
 
 int main() {
-    cout << "Carol Sanders Feb 15, 2020" << endl;
+    cout << "Carol Sanders Feb 16, 2020" << endl;
+    int sortArr[] = {1,2,79,4,19,26,45,3,9,18,25,34,56,48,55,91,2001,2010,156};
 
     LIST L1; // Step 2 - Declare instance of your new class / data type /
 
@@ -153,6 +224,7 @@ int main() {
     L1.printAll();
 
     L1.userInput();
+    L1.mergeSort( sortArr[]);
 
     if ( L1.search(17) ) { cout << "17 Found" << endl; }
     else { cout << "17 not found"<< endl; }
@@ -165,3 +237,15 @@ int main() {
     return 0;
 }
 
+/* MergeSort(headRef)
+1) If the head is NULL or there is only one element in the Linked List
+    then return.
+2) Else divide the linked list into two halves.
+      FrontBackSplit(head, &a, &b); // a and b are two halves
+3) Sort the two halves a and b.
+MergeSort(a);
+MergeSort(b);
+4) Merge the sorted a and b (using SortedMerge() discussed here)
+and update the head pointer using headRef.
+*headRef = SortedMerge(a, b);
+*/
