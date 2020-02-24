14e04da0d760e93e14ed60ffe8237795941b2bfc
Carol Sanders <carol@carolsanders.org>
2020-02-24 01:03:02 -0800

    Add PrStack

--- main.cpp
+++ main.cpp
@@ -6,59 +6,49 @@ Take a look at this and start with it...
  */
 
 #include <iostream>
-#include <cstdio>
 #include <stack>
 
 using namespace std;
 
-class StackOfWords {
-private:
-    stack <string> listOfWords;
-
-public:
-    void userInput() {
-        int input = 0;
-        stack <string> ListStack;
-        string tempVal;
-        restart:
-            cout << "Enter option:\n 1.Add\n 2.Remove\n 3.Print\n 4.Node Count\n 5.Exit Program" << endl;
-            cin >> input;
-        switch (input) {
-            case 1:
-                cout << "Add word to list: ";
-                cin >> tempVal;
-                ListStack.push(tempVal);
-                goto restart;
-            case 2:
-                cout << "Removing the value: ";
-                cin >> tempVal;
-                ListStack.pop();
-                goto restart;
-            case 3:
-                cout << "Enter value to search: \n";
-                cin >> tempVal;
-//                ListStack::search(tempVal);
-                goto restart;
-            case 4:
-                cout << "Printing all the stored values: \n";
-                ListStack.top();
-                goto restart;
-            case 5:
-                cout << "Number of words: " << ListStack.size() << endl;
-                goto restart;
-            case 6:
-                cout << "Exit Program" << endl;
-                break;
-            default:
-                goto restart;
-        }
+void userInput() {
+    int input = 0;
+    stack <string> ListStack;
+    string tempVal;
+    restart:
+    cout << "Enter option:\n 1.Add\n 2.Remove\n 3 Reverse\n 4.Print Last entered \n 5.Show Size\n 6.Exit Program\n" << endl;
+    cin >> input;
+    switch (input) {
+        case 1:
+            cout << "Add word to list: ";
+            cin >> tempVal;
+            ListStack.push(tempVal);
+            cout << "Value entered: " << ListStack.top() << endl;
+            goto restart;
+        case 2:
+            cout << "Removing the last value entered: "; ListStack.pop();
+            goto restart;
+        case 3:
+            cout << "there is no reverse method in the std stack lib: \n";
+//                ListStack.reverse();  there is no reverse in the stack lib
+            goto restart;
+        case 4:
+            cout << "Printing the last stored value:" << ListStack.top() << endl;
+            goto restart;
+        case 5:
+            cout << "Number of words: " << ListStack.size() << endl;
+            goto restart;
+        case 6:
+            cout << "Exit Program" << endl;
+            break;
+        default:
+            goto restart;
     }
-};
+}
+
 
 int main() {
-    cout << "Carol Sanders Stack stadnrd methods " << endl; // Test all access methods
-    StackOfWords s1;
-    s1.userInput();
+    cout << "Carol Sanders Stack  methods " << endl; // Test all access methods
+    userInput();
 
     system("pause");  //not needed on the Mac
     return 0;
